1d605de0b4cf5e521d0ed166d21183c695edc40d
Nukr <nukrs.w@gmail.com>
2015-01-05 17:07:59 +0800

    add static on client/build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static('client/build'))
 
 app.get('/friends/:phone', function (req, res) {
     client.smembers("MB:" + req.params.phone + ":registered", function (err, friends) {
