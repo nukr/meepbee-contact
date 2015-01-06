1eeb23ecf5afa4880b608792c9b8b51c4f52cd0f
Nukr <nukrs.w@gmail.com>
2015-01-06 17:42:17 +0800

    add status code to utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -5,25 +5,32 @@ exports.userdatatodb = function (client, user, callback) {
 
     var numInserted = 0;
     var status = {}
-    user.contacts.forEach(function (contact, index) {
-        if (user.phone && contact.phone) {
-            numInserted += 1;
-            client.sadd("MB:" + user.phone + ":registered", contact.phone);
-            client.sadd("MB:" + contact.phone + ":BT", user.phone);
-        } else {
-            if (!status.errContact) {
-                status.errContact = [];
-            }
-            status.errContact.push(index + ':' + contact)
+
+    // 沒 phone 就不用繼續了
+    if (user.phone === undefined) {
+        status.code = 500;
+        callback(status)
+    } else {
+        if (user.contacts !== undefined) {
+            status.contacts = user.contacts.length;
+            user.contacts.forEach(function (contact, index) {
+                if (user.phone && contact.phone) {
+                    numInserted += 1;
+                    client.sadd("MB:" + user.phone + ":registered", contact.phone);
+                    client.sadd("MB:" + contact.phone + ":BT", user.phone);
+                } else {
+                    if (!status.errContact) {
+                        status.errContact = [];
+                    }
+                    status.errContact.push(index + ':' + contact)
+                }
+            });
         }
-    });
+        status.code = 200;
+        status.inserted = numInserted;
+        callback(status)
+    }
 
-    status.code = 200;
-    status.inserted = numInserted;
-    status.status = 'success';
-    status.contacts = user.contacts.length;
-
-    callback(status)
 };
 
 exports.normalize = function (phone) {
