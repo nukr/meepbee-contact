b703b297181d24b937ba3728ae9533a3c9633dff
Nukr <nukrs.w@gmail.com>
2015-01-07 09:42:01 +0800

    add http get test case

--- test/rest_spec.js
+++ test/rest_spec.js
@@ -11,10 +11,10 @@ frisby.create('Insert Empty Data')
 
 frisby.create('Insert Good Data')
     .post('http://localhost:3000/users', {
-        phone: 0961007003,
+        phone: "3345678",
         contacts:[
-            {phone: 0961007004},
-            {phone: 0961007005}
+            {phone: "0961007003"},
+            {phone: "0961007007"}
         ]
     })
     .expectStatus(200)
@@ -26,10 +26,10 @@ frisby.create('Insert Good Data')
 
 frisby.create('Insert Bad Data #1 - Bad Key')
     .post('http://localhost:3000/users', {
-        pho: 0961007003,
+        pho: "0961007003",
         contacts:[
-            {phone: 0961007004},
-            {phone: 0961007005}
+            {phone: "0961007004"},
+            {phone: "0961007005"}
         ]
     })
     .expectStatus(200)
@@ -37,4 +37,18 @@ frisby.create('Insert Bad Data #1 - Bad Key')
     .expectJSON({
         "code": 500
     })
+    .toss();
+
+frisby.create('Get /friends/:phone')
+    .get('http://localhost:3000/friends/3345678')
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON(["0961007003", "0961007007"])
+    .toss();
+
+frisby.create('Get /followme/:phone')
+    .get('http://localhost:3000/followme/0961007003')
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON(["3345678"])
     .toss();
